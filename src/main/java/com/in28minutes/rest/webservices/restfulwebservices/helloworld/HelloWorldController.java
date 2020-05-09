da436949fadcb6c0ba621f2d2a9ae9323a78901b
Ousama <elidrissi.ousama.ma@gmail.com>
2020-05-09 00:00:08 +0000

    Using CrossOrigin annotation on WelcomeController

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,10 +1,11 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-//@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class HelloWorldController {
 
@@ -18,8 +19,8 @@ public class HelloWorldController {
 		return new HelloWorldBean("Hello World - Changed");
 	}
 
-	@GetMapping(path = "/hello-world/path-variable/{name}/{email}")
-	public HelloWorldBean helloWorldPathVariable(@PathVariable String name, @PathVariable String email) {
-		return new HelloWorldBean(String.format("Hello World, %s - %s", name, email));
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
